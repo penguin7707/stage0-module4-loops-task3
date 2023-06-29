2aa28a026e0c18189b6663619732e2870e3b597d
Stas <sizovstanislav10@gmail.com>
2023-06-29 16:19:01 +0700

    m4 t3

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class DigitsSum {
     public void printDigitsSum(int t){
-
+        String l = Integer.toString(t);
+        char[] chars = l.toCharArray();
+        int sum = 0;
+        for(char ch: chars){
+            sum = sum + (int)ch;
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
+++ src/main/java/school/mjc/stage0/loops/task3/GreatestCommonDivisor.java
@@ -2,5 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class GreatestCommonDivisor {
     public void printGCD(int first, int second) {
+        int gcd = 1;
+        int max = first>second ? second:first;
+        for(int i=1; i<=max; i++){
+            if(first%i==0 && second%i==0) gcd=i;
+        }
+        System.out.println(gcd);
     }
 }
