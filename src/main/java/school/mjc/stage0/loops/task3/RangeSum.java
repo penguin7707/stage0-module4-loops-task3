414ba2a9c35286646b98a4a61c84d3cebbbe75c7
Stas <sizovstanislav10@gmail.com>
2023-06-29 16:43:41 +0700

    m4 t3 v2

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -3,7 +3,9 @@ package school.mjc.stage0.loops.task3;
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
         int sum=0;
-        for(int i=firstBoarder;i<+secondBoarder;i++){
+        int min = firstBoarder>secondBoarder?secondBoarder:firstBoarder;
+        int max = firstBoarder>secondBoarder?firstBoarder:secondBoarder;
+        for(int i=min;i<=max;i++){
             sum=sum+i;
         }
         System.out.println(sum);
